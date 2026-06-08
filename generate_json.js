e5c01d707113d4076b2d7d4f77ffada17c2229d7
adriabama06 <adriabama06@gmail.com>
2026-06-08 20:16:30 +0200

    Use generate_json.js to generate ffmpeg-list.json & ffmpeg-list-humanized.json files

---
+++ generate_json.js
@@ -0,0 +1,36 @@
+import { readdirSync, writeFileSync } from "fs";
+import { extname, join } from "path";
+
+const BASE_URL = "https://github.com/adriabama06/FFmpeg-Builds-Archive/raw/refs/heads/main/";
+
+const get_os = (fname) => {
+    if(fname.includes("linux")) return "linux";
+    if(fname.includes("win")) return "windows";
+    throw "Unknown os for " + fname;
+}
+
+const get_arch = (fname) => {
+    if(fname.includes("arm64")) return "arm64";
+    if(fname.includes("64")) return "x64";
+    throw "Unknown arch for " + fname;
+}
+
+let result = {};
+
+const versions = readdirSync(".").filter(f => (/\d\./g).test(f)).reverse();
+
+for (const version of versions) {
+    if(!result[version]) result[version] = {};
+
+    const files = readdirSync(join(".", version)).filter(f => [".tar", ".xz", ".zip", ".rar", ".gz"].includes(extname(f)));
+
+    for (const file of files) {
+        if(!result[version][get_os(file)]) result[version][get_os(file)] = {};
+        if(!result[version][get_os(file)][get_arch(file)]) result[version][get_os(file)][get_arch(file)] = {};
+
+        result[version][get_os(file)][get_arch(file)] = BASE_URL + `${version}/${file}`;
+    }
+}
+
+writeFileSync("ffmpeg-list.json", JSON.stringify({ versions: result }));
+writeFileSync("ffmpeg-list-humanized.json", JSON.stringify({ versions: result }, undefined, 4));
